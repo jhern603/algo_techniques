8f33b6c82c7084c9b03048dd019e3ac8ff8fb68d
Jose Hernandez <43681757+jhern603@users.noreply.github.com>
2022-03-15 12:50:44 -0400

    Numbers displayed in grid pattern

--- src/main/java/Prog18_03/GraphDisplay.java
+++ src/main/java/Prog18_03/GraphDisplay.java
@@ -23,38 +23,44 @@ public class GraphDisplay extends JPanel {
         int labelY = 31;
         int gridWidth = 150;
 
+        //1->2
         drawEdge(
-            g,
-            leftX + width,
-            topY + height / 2,
-            leftX + gridWidth,
-            topY + height / 2);
+                g,
+                leftX + width,
+                topY + height / 2,
+                leftX + gridWidth,
+                topY + height / 2);
+        //1->4
         drawEdge(
                 g,
                 leftX + width / 2,
                 topY + height,
                 leftX + gridWidth,
                 topY + height / 2 + gridWidth);
-
-        // draw vertex 1
-        drawVertex( g, leftX, topY, width, height, labelX, labelY, "1" );
-        drawVertex( g, leftX + gridWidth, topY, width, height, labelX, labelY, "2" );
-        drawVertex( g, leftX + gridWidth, topY + gridWidth, width, height, labelX, labelY, "3" );
+        int numRows = 0;
+        int numColumns = 0;
+        for (int i = 0; i < 20; i++) {
+            numRows += i % 5 == 0 ? 1 : 0;
+            numColumns = i % 5 == 0 ? 0 : i % 5;
+            drawVertex(g, leftX + (gridWidth * numColumns), topY + (gridWidth * numRows), width, height, labelX, labelY, i + 1);
+        }
 
 
     }
 
-private void drawVertex( java.awt.Graphics g, int leftX, int topY, int width, int height, int labelX, int labelY,
-                         String content ){
-    g.setColor( java.awt.Color.ORANGE);
-    g.fillOval( leftX, topY, width, height );
-    g.setColor( java.awt.Color.BLACK);
-    g.drawOval( leftX, topY, width, height );
-    g.setFont(new java.awt.Font( java.awt.Font.SANS_SERIF, java.awt.Font.BOLD, 24));
-    g.drawString(content, leftX + labelX, topY + labelY );
-}
+    private void drawVertex(java.awt.Graphics g, int leftX, int topY, int width, int height, int labelX, int labelY,
+                            int num) {
 
-private void drawEdge(java.awt.Graphics g, int x1, int y1, int x2, int y2) {
+        String content = String.valueOf(num);
+        g.setColor(java.awt.Color.ORANGE);
+        g.fillOval(leftX, topY, width, height);
+        g.setColor(java.awt.Color.BLACK);
+        g.drawOval(leftX, topY, width, height);
+        g.setFont(new java.awt.Font(java.awt.Font.SANS_SERIF, java.awt.Font.BOLD, 24));
+        g.drawString(content, leftX + labelX, topY + labelY);
+    }
+
+    private void drawEdge(java.awt.Graphics g, int x1, int y1, int x2, int y2) {
         g.setColor(java.awt.Color.BLACK);
         g.drawLine(x1, y1, x2, y2);
     }
